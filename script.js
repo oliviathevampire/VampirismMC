ac21f0f351f1ae47a0cb42e185783fe1c37bda36
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-09-08 00:22:21 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Lazy loading for images (optional, since we used "loading='lazy'" in HTML)
+if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[loading="lazy"]');
+    images.forEach(img => {
+        img.src = img.dataset.src;
+    });
+} else {
+    // Fallback for browsers that don’t support `loading="lazy"`
+    // You can include a lazy-loading library here
+}

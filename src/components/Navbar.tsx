cf37d8d061554720d087e2c58e6f11cf2b7f9c6c
OliviaTheVampire <sindrefagerheim53@gmail.com>
2023-09-29 01:35:17 +0200

    more progress

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,18 @@ const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <a className="flex items-center text-white">
-            <img src="/images/logo.png" alt="VampirismMC Logo" className="h-8 w-auto mr-2" /> {/* Added logo image */}
-            VampirismMC
-          </a>
+        <Link href="/" className="flex items-center text-white">
+          <img src="/images/logo.png" alt="VampirismMC Logo" className="h-8 w-auto mr-2" /> {/* Added logo image */}VampirismMC
         </Link>
         <div className="flex space-x-4">
-          <Link href="/">
-            <a className="text-white">Home</a>
+          <Link href="/" className="text-white">
+            Home
           </Link>
-          <Link href="/about">
-            <a className="text-white">About</a>
+          <Link href="/about" className="text-white">
+            About
           </Link>
-          <Link href="/contact">
-            <a className="text-white">Contact</a>
+          <Link href="/contact" className="text-white">
+            Contact
           </Link>
         </div>
       </div>
